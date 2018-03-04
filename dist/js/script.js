b87b7267b87007f7a03171c6caa00f6c45d8cc23
bekirova <viktoriia.bekirova@gmail.com>
2018-03-04 17:18:45 +0700

    Kontur

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,16 @@
-// для всплывающих подсказок
-$(document).ready(function () {
-    $('[data-toggle="tooltip"]').tooltip();
+$(function () {
+    $('.btn.btn-row1').on('click', function () {
+        $(this).css('background-color','#24afb2');
+    });
+});
+
+$(function () {
+    $('.dropdown.show').on('click', function () {
+        $('.list').slideToggle (300, function () {
+            if ($(this).css('display') === 'none') {
+                $(this).removeAttr('style');
+            }
+        });
+    });
 });
 
